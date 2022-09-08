46b504fa8f752979cee97951a3481fb90b2bc061
mohit-cfc <santosh.mohit9@gmail.com>
2022-09-08 12:37:20 +0530

    Added Tables with sorting

--- form.js
+++ form.js
@@ -1,1 +1,8 @@
 const form = document.querySelector("#form");
+console.log("Hello");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  for (let data of form.elements) {
+    console.log(data.value);
+  }
+});
